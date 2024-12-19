ddbaa7ad99e1e2293ac715259c00a37f1be5de2f
Braian Manrique <braianm@iadb.org>
2024-12-19 15:01:40 -0500

    actualizacion en pagina para creacion de citas prioritarias

--- src/app/pages/citas-prioritarias/citas-prioritarias.component.ts
+++ src/app/pages/citas-prioritarias/citas-prioritarias.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { CreateAppointmentFormComponent } from '../../shared/componets/dialog/create-appointment-form/create-appointment-form.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-citas-prioritarias',
   templateUrl: './citas-prioritarias.component.html',
-  styleUrl: './citas-prioritarias.component.scss'
+  styleUrls: ['./citas-prioritarias.component.scss']
 })
 export class CitasPrioritariasComponent {
+  role: string = 'admin';
+  constructor(private dialog: MatDialog) {}
 
+ 
+openCreateAppointmentDialog() {
+  const dialogRef = this.dialog.open(CreateAppointmentFormComponent, {
+    width: '600px',
+    maxHeight: '90vh', 
+    disableClose: true,
+  });
+
+  dialogRef.afterClosed().subscribe(result => {
+    if (result) {
+      console.log('Datos recibidos del modal:', result);
+    }
+  });
+}
 }

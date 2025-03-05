2bee74303dd68e6397620493a3a4b641ee5749ef
Braian Manrique <braianm@iadb.org>
2025-03-04 22:32:31 -0500

    update dashboard page

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,72 +2,83 @@ import { Component } from '@angular/core';
 import { UsuarioService } from '../../services/usuario.service';
 import { User } from '../../interfaces/user.interface';
 import { LoadingService } from '../../services/shared/loading.service';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReportesService } from '../../services/reportes/reportes.service';
+import moment from 'moment';
 export interface Funcionario {
-  id: number;  // Asegúrate de usar `number` para ID
+  id: number;
   nombre: string;
-  caja: string;  // Caja o puesto en el que está trabajando
+  caja: string;
 }
-
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
-  // funcionarios: Funcionario[] = FUNCIONARIOS_MOCK;  // Datos mock de funcionarios
-  userLoginOn:boolean = false;
-  userData?:User ;
+  userLoginOn: boolean = false;
+  userData?: User;
   selectedFuncionario: Funcionario | null = null;
   userName: string = '';
   firstName: string = '';
   userRole: string = '';
   userUsername: string = '';
-  funcionarios = [
-    { id: 1, nombre: 'Juan Pérez', caja: 'Caja 1' },
-    { id: 2, nombre: 'Ana Gómez', caja: 'Caja 2' },
-    { id: 3, nombre: 'Carlos López', caja: 'Caja 3' },
-    // Agrega más funcionarios según necesidad
-  ];
-  constructor(private loginService: UsuarioService,  private loadingService: LoadingService) {
-    
-  }
-  onFuncionarioSelect(event: any): void {
-    const selectedId = event.value;
-    this.selectedFuncionario = this.funcionarios.find(f => f.id === selectedId) || null;
+
+  reporteForm: FormGroup;
+
+  reporteCitas: any = null;
+
+  constructor(
+    private loginService: UsuarioService,
+    private loadingService: LoadingService,
+    private fb: FormBuilder,
+    private reportesService: ReportesService
+  ) {
+    this.reporteForm = this.fb.group({
+      fecha: [moment().toDate(), Validators.required],
+    });
   }
 
-  ngOnDestroy():void{
+  ngOnDestroy(): void {
     this.loginService.currentUserData.unsubscribe();
     this.loginService.currentUserLoginOn.subscribe();
   }
-  
-  ngOnInit(): void{
 
+  ngOnInit(): void {
     const userData = JSON.parse(localStorage.getItem('user') || '{}');
-    this.firstName = `${userData.first_name}`  || '...';
+    this.firstName = `${userData.first_name}` || '...';
     this.userRole = userData.rol || 'Sin rol definido';
     this.userUsername = userData.username || 'Sin usuario';
-    this.loadingService.hide(); 
-
+    this.loadingService.hide();
 
     this.loginService.currentUserLoginOn.subscribe({
-      next:(userLoginOn) =>{
+      next: (userLoginOn) => {
         this.userLoginOn = userLoginOn;
-
-      }
+      },
     });
 
     this.loginService.currentUserData.subscribe({
-      next:(userData) => {
-        console.log(userData,'userLoginOn')
+      next: (userData) => {
+        console.log(userData, 'userLoginOn');
 
         this.userData = userData;
-      }
-    })
-
-
+      },
+    });
   }
 
+  consultarCitas(): void {
+    const fechaSeleccionada = moment(this.reporteForm.value.fecha).format(
+      'DD/MM/YYYY'
+    );
+
+    this.reportesService.getReporteCitasDia(fechaSeleccionada).subscribe({
+      next: (response: any) => {
+        this.reporteCitas = response;
+      },
+      error: (error) => {
+        console.log(error, 'err');
+      },
+    });
+  }
 }

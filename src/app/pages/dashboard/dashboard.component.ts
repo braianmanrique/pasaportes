c9a71f9c800099b5d1d744a9bbe839656e803a42
Braian Manrique <braianm@iadb.org>
2025-06-02 15:50:41 -0500

    add image catira

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -5,6 +5,7 @@ import { LoadingService } from '../../services/shared/loading.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ReportesService } from '../../services/reportes/reportes.service';
 import moment from 'moment';
+import { MatSnackBar } from '@angular/material/snack-bar';
 export interface Funcionario {
   id: number;
   nombre: string;
@@ -28,12 +29,18 @@ export class DashboardComponent {
   reporteForm: FormGroup;
 
   reporteCitas: any = null;
-
+  buscarCedulaForm = this.fb.group({
+    cedula: ['', [Validators.required, Validators.minLength(5)]],
+  });
+  resultadoBusqueda: any[] = [];
+  yaBusco = false;
+  resultadoCedula: any = null;
   constructor(
     private loginService: UsuarioService,
     private loadingService: LoadingService,
     private fb: FormBuilder,
-    private reportesService: ReportesService
+    private reportesService: ReportesService,
+    private snackBar: MatSnackBar
   ) {
     this.reporteForm = this.fb.group({
       fecha: [moment().toDate(), Validators.required],
@@ -66,6 +73,57 @@ export class DashboardComponent {
       },
     });
   }
+  limpiarBusquedaCedula() {
+    this.buscarCedulaForm.reset();        
+    this.resultadoBusqueda = [];            
+    this.yaBusco = false;                   
+
+  }
+
+  get cedula() {
+    return this.buscarCedulaForm.get('cedula')!;
+  }
+  
+  buscarPorCedula() {
+    debugger
+    const cedula = this.buscarCedulaForm.get('cedula')?.value;
+    if (!cedula) return;
+  
+    if (!cedula || this.buscarCedulaForm.invalid ) {
+      // Aquí evitas ejecutar la búsqueda si la cédula no es válida
+      return;
+    }
+  
+    const url = `https://backend-auth-log-project.onrender.com/api/usuarios/reporte_citas_pasaportes_estados_completo/?cedula=${cedula}`;
+    this.reportesService.getInfoPorCedula(url).subscribe({
+      next: (data) => {
+        if (data) {
+          const citasAsignadas = data.citas_asignadas || [];
+          const citasPrioritarias = data.citas_prioritarias || [];
+          this.resultadoBusqueda = [...citasAsignadas, ...citasPrioritarias];
+  
+          this.yaBusco = true;
+  
+          if (this.resultadoBusqueda.length > 0) {
+            this.snackBar.open('Persona encontrada con éxito.', 'Cerrar', {
+              duration: 3000,
+            });
+          } else {
+            this.snackBar.open('No se encontraron citas para esta cédula.', 'Cerrar', {
+              duration: 3000,
+            });
+          }
+        }
+      },
+      error: (err) => {
+        console.error('Error al buscar por cédula:', err);
+        this.resultadoBusqueda = [];
+        this.yaBusco = true;
+        this.snackBar.open('Ocurrió un error al consultar.', 'Cerrar', { duration: 3000 });
+      },
+    });
+  }
+  
 
   consultarCitas(): void {
     const fechaSeleccionada = moment(this.reporteForm.value.fecha).format(
